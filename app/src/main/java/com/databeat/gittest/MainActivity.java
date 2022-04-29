db048ed6cb8e795324a331c51a392ed63b3e51d4
Mani <mani.gupta@ashtbit.com>
2022-04-29 13:02:38 +0530

    second commit

--- app/src/main/java/com/databeat/gittest/MainActivity.java
+++ app/src/main/java/com/databeat/gittest/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        // this is a test message
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
